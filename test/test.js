c3f53050375aa44a6dfcdd3303f95bcb8ff5a979
myopenleder <cnu.srini@hotmail.com>
2018-12-22 08:52:40 -0500

    included test cases for setting owners

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ let Auction = artifacts.require("./Auction.sol");
 var assert = require('assert');
 
 let auctionInstance;
+let defaultAddress = '0x0000000000000000000000000000000000000000';
 
 contract('AuctionContract', function (accounts) {
   //accounts[0] is the default account
@@ -84,5 +85,83 @@ it("Should NOT allow non owner to reveal winners", function() {
      }
    })
  });
+
+ it("Should set winners", function() {
+    /**********
+    TASK 7: Call register function from account 2
+    ***********/
+    return auctionInstance.register({from: accounts[2]})
+    .then(function(result) {
+      /**********
+      TASK 8: Call register function from account 3
+      ***********/
+        return auctionInstance.register({from: accounts[3]})
+    }).then(function() {
+      /**********
+      TASK 9: Call register function from account 4
+      ***********/
+        return auctionInstance.register({from: accounts[4]})
+    }).then(function() {
+      /**********
+      TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
+      pass itemId=0, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(0, 5 , {from: accounts[2]})
+    }).then(function() {
+      /**********
+      TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
+      pass itemId=1, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(1, 5, {from: accounts[3]})
+    }).then(function() {
+      /**********
+      TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
+      pass itemId=2, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(2, 5, {from: accounts[4]})
+    }).then(function() {
+      /**********
+      TASK 13: Call revealWinners function from accounts[0]
+      ***********/
+        return auctionInstance.revealWinners({from: accounts[0]})
+    }).then(function() {
+      /**********
+      TASK 14: call winners function from accounts[0] to get the winner of item id 0
+      ***********/
+        return auctionInstance.winners(0, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 15:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result , defaultAddress , 'winner address is not the default address');
+      /**********
+      TASK 16: call winners function from accounts[0] to get the winner of item id 1
+      ***********/
+      return auctionInstance.winners(1, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 17:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result , defaultAddress , 'winner address is not the default address');
+      /**********
+      TASK 18: Call winners function from account 3 to get the winner of item id 2
+      ***********/
+      return auctionInstance.winners(2, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 19:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result , defaultAddress , 'winner address is not the default address');
+    })
+  });
 
 });
